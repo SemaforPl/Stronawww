504b1baac822b1bed57ea2475541a0532b9ba34a
KapiCytryna <kacperczura9@gmail.com>
2022-05-24 22:18:16 +0200

    naprawiłem ci nav ziom

--- app.js
+++ app.js
@@ -76,3 +76,12 @@ POPUP.addEventListener("click", (e) => {
                        )
 
 ;
+
+function myFunction() {
+    let x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
